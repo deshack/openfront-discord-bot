0896e552c0d3b40728cb6b3b88282dd7f90bff95
Mattia Migliorini <mattia@visiaquantum.com>
2026-02-17 11:44:39 +0100

    Revert "Extend clan wins period for test"
    This reverts commit 134187eb5b208c7e0b5cf9991f20c267c7010fcf.

--- src/handlers/scheduled.ts
+++ src/handlers/scheduled.ts
@@ -317,7 +317,7 @@ async function handleClanWins(env: Env): Promise<void> {
   }
 
   const now = new Date();
-  const startDate = new Date(now.getTime() - 12 * 60 * 60 * 1000);
+  const startDate = new Date(now.getTime() - 4 * 60 * 60 * 1000);
   const start = startDate.toISOString();
   const end = now.toISOString();
 
